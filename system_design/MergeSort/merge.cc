1f7ab3062df6875dbc771f32adbcef2258f21eba
Hobum <hobum.kwon@gmail.com>
2016-02-08 16:06:00 -0800

    still no

---
+++ system_design/MergeSort/merge.cc
@@ -0,0 +1,43 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  merge.cc
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/08/2016 03:48:45 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Hobum (Vincent) Kwon (mn), hobum.kwon@gmail.com
+ *        Company:  Samsung Electornics
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+void merge(int s, int e, int* a) {
+  cout << "enter: " << s << "," << e << endl;
+  if (s < e) {
+    int mid = s + (e-s+1)/2;
+    merge(s, mid, a);
+    merge(mid+1, e, a);
+  } else {
+    return;
+  }
+}
+
+int main() {
+  int a[5];
+  int *aptr = &a[0];
+
+  for (int i = 0; i < 5 ; i++) {
+    cin >> a[i] ; 
+  }
+
+  merge(0, 4, aptr); 
+}
