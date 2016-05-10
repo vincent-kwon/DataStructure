33fe3c2c66b3aa2d9ac7575ce9dc8f560a0f7435
vincent <vincent@gwon-ui-Mac-mini.local>
2016-05-11 00:37:09 +0900

    add bidir

--- system_design/Graph/29.8.hanoi.cc
+++ system_design/Graph/29.8.hanoi.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -13,7 +14,7 @@ int set(int state, int index, int value) {
   return (state & ~(3<<(index*2))) | (value << (index * 2));
 }
 
-int c[1<<(MAX_DISC*2)];
+int c[1<<(MAX_DISC*2)]; //
 
 int sgn(int x) {
   if (!x) return 0; return x > 0 ? 1 : -1; 
@@ -24,14 +25,15 @@ int incr(int x) {
 }
 
 int bidir(int disc, int begin, int end) {
+  // check same
   if (begin == end) return 0;
 
   queue<int> q;
   
-  memset(c, 0, sizeof(c));
+  memset(c, 0, sizeof(c)); // array c has 
 
-  q.push(begin); c[begin] = 1;
-  q.push(end); c[end] = -1;
+  q.push(begin); c[begin] = 1; // normal direction
+  q.push(end); c[end] = -1; // reverse direction
 
   while (!q.empty()) {
     int here = q.front();
@@ -40,18 +42,21 @@ int bidir(int disc, int begin, int end) {
     for (int i = disc-1; i >= 0; i--) {
       top[get(here, i)] = i;
     }
-    for (int i = 0; i < 4; i++) 
-      if (top[i] != -1)
-        for (int j = 0; j < 4; j++) 
+    for (int i = 0; i < 4; i++) {
+      if (top[i] != -1) {
+        for (int j = 0; j < 4; j++) {
           if (i != j && (top[j] == -1 || top[j] > top[i])) {
             int there = set(here, top[i], j);
             if (c[there] == 0) {
               c[there] = incr(c[here]);
               q.push(there);
             }
+            else if (sgn(c[there] != sgn(c[here])))
+              return abs(c[there]) + abs(c[here]) - 1;
           }
-          else if (sgn(c[there] != sgn(c[here]))
-            return abs(c[there]) + abs(c[here]) - 1;
+        }
+      }
+    }
   }
   return -1;
 }
@@ -62,12 +67,12 @@ int bfs(int disc, int begin, int end) {
   memset(c, -1, sizeof(c));
   q.push(begin);
   c[begin] = 0;
-  while (!.q.empty()) {
+  while (!q.empty()) {
     int here = q.front();
     q.pop();
     int top[4] = { -1, -1, -1, -1 };
     for (int i = disc-1; i >= 0; --i) {
-      top[get(here, i)] = i; 
+      top[get(here, i)] = i; // current top of this pole
       for (int i = 0; i < 4; i++) {
         if (top[i] != -1) { 
           for (int j = 0; j < 4; j++) {
@@ -87,5 +92,46 @@ int bfs(int disc, int begin, int end) {
 }
 
 int main() {
+  int test;
+  cin >> test;
+  while(test-- > 0) {
+    int inputSize[4];
+    int inputArray[4][12];
+    int outputSize[4];
+    int outputArray[4][12];
+ 
+    int num;
+    cin >> num;
+    int beginVal = 0, endVal = 0;
 
+    for (int pole = 0 ; pole < 4 ; pole++) 
+    {
+      cin >> inputSize[pole]; 
+
+      for (int j = 0; j < inputSize[pole]; j++) 
+      {
+        cin >> inputArray[pole][j];
+        beginVal = set(beginVal, inputArray[pole][j], pole);
+      }
+
+    }
+    cout << "input " << beginVal << endl;
+
+    // with this info, what can you do more?
+
+    for (int i = 0 ; i < 4 ; i++) 
+    {
+      cin >> outputSize[i];
+
+      for (int j = 0; j < outputSize[i]; j++) 
+      {
+        cin >> outputArray[i][j];
+        endVal = set(endVal, outputArray[i][j], i);
+      }
+    }
+    cout << "output " << endVal << endl;
+    int ret = bidir(num, beginVal, endVal);
+    cout << "ret: " << ret << endl;
+  }  
+  return 0;
 }
