66861c4a9d95dbee6e429a037752ef796954eff6
Hobum <hobum.kwon@gmail.com>
2016-08-02 19:35:40 +0900

    Direction added

--- Algorithm3/Backtracking/FromCornerCellToMiddleInMaze/dirCornerObject.cc
+++ Algorithm3/Backtracking/FromCornerCellToMiddleInMaze/dirCornerObject.cc
@@ -22,10 +22,10 @@
 
 using namespace std;
 
-#define N 5 
+#define N 3
 
 
-int maze[N][N] = {
+int maze[5][5] = {
   {1, 1, 1, 1, 1,},
   {1, 1, 1, 1, 1,},
   {1, 1, 1, 1, 1,},
@@ -33,21 +33,72 @@ int maze[N][N] = {
   {1, 1, 1, 1, 1,},
 };
 
-int row[] =  { -1, 1,  0, 0 };
-int col[] =  {  0, 0, -1, 1 };
-int _row[] = {  0,   0, N-1, N-1 };
-int _col[] = {  0, N-1,   0, N-1 };
+//int row[] =  { -1, 1,  0, 0 };
+//int col[] =  {  0, 0, -1, 1 };
+
+enum TO_DIR {
+  TO_RIGHT,
+  TO_LEFT, 
+  TO_BOTTOM,
+  TO_TOP
+};
+
+enum FROM_DIR {
+  FROM_LEFT,
+  FROM_RIGHT,
+  FROM_TOP,
+  FROM_BOTTOM
+};
+
+
+int row[4][2] = { 
+ { 0, 1 }, // from left to right, bottom
+ { 0, -1 },   // from right to left, top
+ { 1, 0 }, // from top to down, left
+ { -1, 0 }, // from bottom to up, right
+};
+
+int col[4][2] = { 
+ { 1, 0 }, // from left
+ { -1, 0},   // from right
+ { 0, -1 }, // from top
+ { 0, 1 }, // from bottom
+};
+
+int fromDir[4][2] = {
+  {FROM_LEFT, FROM_TOP},
+  {FROM_RIGHT, FROM_BOTTOM},
+  {FROM_TOP, FROM_RIGHT },
+  {FROM_BOTTOM, FROM_LEFT }, 
+};
+
+//int _row[] = { 0, 0  , N-1, N-1 };
+//int _col[] = { 0, N-1, 0,   N-1 };
 
 bool isValid(set<pair<int, int> > visited, pair<int, int> pt) {
   return (pt.first >= 0) && (pt.first < N) &&
          (pt.second >= 0) && (pt.second < N) &&
          (visited.find(pt) == visited.end());
-            
 }
 
-void printPath(list<pair<int, int> > path) {
-  for (auto it = path.begin(); it != path.end(); it++) {
-    cout << "(" << it->first << " , " << it->second << ") -> ";
+void printPath(list<pair<int, int> > path, list<int> fromPath) {
+
+  auto fromIt = fromPath.begin();
+
+  for (auto it = path.begin(); it != path.end(); it++, fromIt++) {
+    string dirStr;
+    //cout << "PP:" << *fromIt << endl;
+    switch (*fromIt) {
+      case FROM_LEFT: dirStr = "FROM LEFT";
+                 break;
+      case FROM_RIGHT: dirStr = "FROM RIGHT";
+                 break;
+      case FROM_TOP: dirStr = "FROM TOP";
+                 break;
+      case FROM_BOTTOM: dirStr = "FROM BOTTOM";
+                 break;
+    }
+    cout << "DIR: " << dirStr <<" (" << it->first << " , " << it->second << ") -> " << endl;
   }
 
   cout << "MID" << endl;
@@ -55,31 +106,33 @@ void printPath(list<pair<int, int> > path) {
 
 void findPathInMazeUtil(list<pair<int, int> > &path,
                         set<pair<int, int> > &visited,
-                        pair<int, int> &curr) {
+                        pair<int, int> &curr, int from_dir, 
+                        list<int>& fromPath) {
+
   if (curr.first == N/2 && curr.second == N/2) 
   {
-    printPath(path);
+    printPath(path, fromPath);
     return;
   }
 
-  for (int i = 0; i < 4; ++i) {
+  for (int i = 0; i < 2; ++i) {
     int n = maze[curr.first][curr.second];
-    int x = curr.first + row[i] * n;
-    int y = curr.second + col[i] * n;
+    int x = curr.first  + row[from_dir][i] * n;
+    int y = curr.second + col[from_dir][i] * n;
     pair<int, int> next = make_pair(x,y);
 
     if (isValid(visited, next)) {
       visited.insert(next);
       path.push_back(next);
-      findPathInMazeUtil(path, visited, next);
+      fromPath.push_back(fromDir[from_dir][i]);
+      findPathInMazeUtil(path, visited, next, fromDir[from_dir][i], fromPath);
       path.pop_back(); 
+      fromPath.pop_back();
       visited.erase(next);
     }
     else {
       if (n  == 0) {
-        //cout << "Not Valid Path: starting... " << endl;
-        printPath(path);
-        //cout << "End path...." << endl;
+        //printPath(path, );
       }
     }
   }
@@ -87,20 +140,18 @@ void findPathInMazeUtil(list<pair<int, int> > &path,
 
 void findPathInMaze() {
   list<pair<int, int> > path;
+  list<int> fromPath;
   set<pair<int, int> > visited;
-
-  for (int i = 0; i < 4; ++i) {
-    int x = _row[i];
-    int y = _col[i];
-
-    pair<int, int> pt = make_pair(x,y);
-
-    visited.insert(pt);
-    path.push_back(pt); // insert to path
-    findPathInMazeUtil(path, visited, pt); 
-    path.pop_back();    // remove from path  
-    visited.erase(pt); // for next corner
-  }  
+  int x = 0;
+  int y = 0;
+  pair<int, int> pt = make_pair(x,y);
+  visited.insert(pt);
+  path.push_back(pt); // insert to path
+  fromPath.push_back(FROM_LEFT);
+  findPathInMazeUtil(path, visited, pt, FROM_LEFT, fromPath); 
+  path.pop_back();    // remove from path  
+  fromPath.pop_back();
+  visited.erase(pt); // for next corner
 }
 
 int main() {
