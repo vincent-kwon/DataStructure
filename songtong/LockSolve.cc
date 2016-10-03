bfea2984053c88b491d54f9095da2e17aa5d75cd
Hobum <hobum.kwon@gmail.com>
2016-10-03 19:36:02 +0900

    Sort out dirs again

--- songtong/LockSolve.cc
+++ songtong/LockSolve.cc
@@ -1,9 +1,37 @@
+/*
+ * 어떤 자물쇠가 있다
+ *
+ * N 자리수 이다 
+ *
+ * 최대 세 자물쇠를 동시에 움직일 수 있다
+ *
+ * 최소 움직임으로 정답을 찾아야 한다
+ *
+ * 예를 들어
+ *
+ * 3456
+ *
+ * 6789 
+ *
+ * 면 2번이다 
+ *
+ * 돌릴 수 있는 최대는 +3, -3 이다
+ *
+ * N은 50 자리 까지 가능하다
+ *
+ * 어떻게 할 것인가?
+ *
+ *
+ */
 #include <iostream>
 #include <math.h>
 #include <stdlib.h>
 
+#define ASCII_ZERO 48
+
 char a[51];
 char t[51];
+int digit;
 
 using namespace std;
 
@@ -18,30 +46,25 @@ int doubleTurn[3][4][2] =
 
 void move(int index, int gap) {
   int newValue;
-  int value = a[index]-48;
-  //cout << "to move: " << a[index] << " as value " << value << endl;
+  int value = a[index]-ASCII_ZERO;
+
   if (gap > 0) {
     int t = 10 - value;
-    if (gap > t) {
-      newValue = gap - t;  // 
-    } 
+    if (gap > t) newValue = gap - t; 
     else newValue = value + gap;
   }
-
   else {
-    if (abs(gap) > value) {
-      newValue = 10 - (abs(gap) - value);
-    } else newValue = value - abs(gap);
+    if (abs(gap) > value) newValue = 10 - (abs(gap) - value);
+    else newValue = value - abs(gap);
   }
-  a[index] = (char) (newValue + 48);
+  a[index] = (char) (newValue + ASCII_ZERO);
 }
 
 int getGap(int current, int goal) {
   int tmp; 
   
-  if (goal > current) {
-    tmp = goal - current; // 9 - 1 = 8 
-  } 
+  if (goal == current) return 0;
+  else if (goal > current) tmp = goal - current; // 9 - 1 = 8 
   else {                        // 9,8,7 need to be changed
     tmp = current - goal;
     tmp = 10 - tmp; // 9 - 8 , 10 - 1 = 9 
@@ -55,38 +78,39 @@ int getGap(int current, int goal) {
 }
 
 void turn(int start, int end, int gap) {
-  for (int i = start; i <= end; i++) {
-    move(i, gap);
-  }
+  for (int i = start; i < digit && i <= end; i++) move(i, gap);
 }
 
 int solve(int index) {  
   int curr = a[index];
   int next = a[index+1];
   int r;
-  int m = dp[index][curr][next];
+  int m = dp[index][curr-ASCII_ZERO][next-ASCII_ZERO];
   int g = getGap(curr, t[index]); // g can be + or - 
-
-  if (m != -1) return m;
   
-  if (abs(g) == 0) {
-    m = solve(index+1);     
-    if (dp[index][curr][next] == -1 || dp[index][curr][next] > r)
-      dp[index][curr][next] = r;
-  }
+  //cout << "solve: " << a << " , index : " << index << " , curr : " << (char) curr << " gap: " << g << endl;
+  if (m != -1) return m;
 
-  else if (abs(g) < 3) {
-    int currentSmall = 987654321;
+  if (index == digit-1) {
+     if (g == 0) return 0;
+     else if (g == 7 || g == 8 || g == 9) return 2;
+     else return 1;  
+  }
+  
+  if (abs(g) == 0) m = solve(index+1);     
+
+  else if (abs(g) <= 3) {
+    m = 987654321;
     for (int digitEnd = 0; digitEnd < 3; digitEnd++) {
       turn(index, index+digitEnd, g); // rotate
       int tmp = solve(index+1)+1;
       turn(index, index+digitEnd, -g); // back
-      currentSmall = ((currentSmall>tmp)?tmp:currentSmall);
+      m = ((m>tmp)? tmp : m);
     }
-    m = currentSmall;
   }
 
   else {
+    m = 987654321;
     for (int j = 0; j < 3; j++) { // 1번 째 디스크에 대한 회전
       for (int k = 0; k < 4; k++) {
         turn(index, index+j, doubleTurn[g-4][k][0]);
@@ -101,29 +125,22 @@ int solve(int index) {
     }
   }
 
-  dp[index][curr][next] = m;
+  dp[index][curr-ASCII_ZERO][next-ASCII_ZERO] = m;
+  cout << "dp["<< index << "][" << curr-ASCII_ZERO << "][" << next-ASCII_ZERO << "] : " << m << endl;
   return m;
 }
 
 int main() {
-  int digit ;
-
   cin >> digit;
   cin >> a;
   cin >> t;
-  cout << a << endl;
-  cout << t << endl;
 
-
-  // dp initialization
+  for (int i = 0 ; i < 50; i++) 
+    for (int j = 0 ; j < 10; j++)
+      for (int k = 0; k < 10; k++)
+        dp[i][j][k] = -1; 
   int r = 0;
-  
-  move(0, -2);
-  //solve(0);
-  cout << a << endl;
+  r = solve(0);
   cout <<"1#" << r << endl;
-  cout << "gap 0, 9 " << getGap(0, 9) << endl;
-  cout << "gap 9, 0 " << getGap(9, 0) << endl;
-  cout << "gap 1, 7 " << getGap(1, 7) << endl;
   return 0;
 }
